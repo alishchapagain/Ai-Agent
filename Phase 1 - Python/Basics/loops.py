fbebd59bcac211f4dffa356bba0531b7e0e3f9f7
Alish Chapagain <alishchapagain2580@gmail.com>
2026-01-02 20:25:13 +0545

    Loops in Python

---
+++ Phase 1 - Python/Basics/loops.py
@@ -0,0 +1,32 @@
+# 1. For loop
+'''
+Syntax:
+for i in range(start,stop,steps): (By default step is 1)
+    statement
+'''
+#Example
+for i in range(0,6):
+    print(i)
+    i+=1
+
+# 2. While loop
+'''
+Syntax:
+while condition:
+    Body
+    increment/decrement
+'''
+#Example
+name=input("Enter your Name: ")
+x=0
+
+while x<=10:
+    print(name)
+    x=x+1
+
+# printing 1 to 10
+y=1
+while y<=10:
+    print(y)
+    y=y+1
+
